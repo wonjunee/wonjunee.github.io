6cfae834d4a9bf61e16a9c2cf43a4fc778e5e27a
Wonjun Lee <mymamyma@gmail.com>
2022-06-17 23:11:42 -0700

    Add files via upload

--- html_replace.py
+++ html_replace.py
@@ -4,7 +4,9 @@ with open("info.json") as F:
 
 files_dir = ['.', 'research','tutoring','past_teaching', 'publications', 'research/gradient-flow', 'research/mean-field-games']
 
+print("XXX Processing XXX")
 for file_dir in files_dir:
+	print(file_dir)
 	html = ""
 	with open("{}/body.txt".format(file_dir)) as F:
 		for i in F:
@@ -12,8 +14,8 @@ for file_dir in files_dir:
 				key = i.split('%')[1]
 				html += dic[key]
 			else:
-				print(i)
 				html += i
 
 	with open("{}/index.html".format(file_dir),"w") as F:
 		F.write(html)
+print("XXX Done! XXX")
